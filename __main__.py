c1e731925864e91b1399aab1236bd383f4a74635
Kevin Glinski <KevinGlinski@users.noreply.github.com>
2020-09-22 21:22:46 -0400

    fix ip6 tables

--- __main__.py
+++ __main__.py
@@ -426,7 +426,9 @@ def configure_iptables():
         'ip6tables -A INPUT -i lo -j ACCEPT',
         'ip6tables -A OUTPUT -o lo -j ACCEPT' 
         'ip6tables -A INPUT -s ::1 -j DROP ',
-        'ip6tables -P INPUT DROP# ip6tables -P OUTPUT DROP# ip6tables -P FORWARD DROP',     
+        'ip6tables -P INPUT DROP',
+        'ip6tables -P OUTPUT DROP',
+        'ip6tables -P FORWARD DROP',     
         'ip6tables-save'
     ])
 
